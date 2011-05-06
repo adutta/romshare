d510491aabd0be54f7f6697e60ec379e4b0ccb5c
Koushik Dutta <koushd@gmail.com>
2011-05-06 09:49:52 -0700

    check host

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ keygrip = require('keygrip');
 mysql = new require('mysql').Client();
 path = require('path');
 
+
+mysql.host = process.env.DEPLOYFU_MYSQL_HOST == null ? 'localhost' : process.env.DEPLOYFU_MYSQL_HOST;
 mysql.user = process.env.DEPLOYFU_MYSQL_USER == null ? 'root' : process.env.DEPLOYFU_MYSQL_USER;
 mysql.password = process.env.DEPLOYFU_MYSQL_PASSWORD == null ? 'nignog' : process.env.DEPLOYFU_MYSQL_PASSWORD;
-mysql.connect();
+mysql.connect(function(err) {
+  if (err)
+    console.log(err);
+});
 
 mysql.query(sprintf('use %s', process.env.DEPLOYFU_MYSQL_DATABASE == null ? 'romshare' : process.env.DEPLOYFU_MYSQL_DATABASE));
 
