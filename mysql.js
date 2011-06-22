2bf89664a1e223547a65527f6c0aee4d3ae6c204
Koushik Dutta <koushd@gmail.com>
2011-06-22 14:17:43 -0700

    no default password

--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@ mysql = new require('mysql').Client();
 
 mysql.host = process.env.DEPLOYFU_MYSQL_HOST == null ? 'localhost' : process.env.DEPLOYFU_MYSQL_HOST;
 mysql.user = process.env.DEPLOYFU_MYSQL_USER == null ? 'root' : process.env.DEPLOYFU_MYSQL_USER;
-mysql.password = process.env.DEPLOYFU_MYSQL_PASSWORD == null ? 'nignog' : process.env.DEPLOYFU_MYSQL_PASSWORD;
+mysql.password = process.env.DEPLOYFU_MYSQL_PASSWORD == null ? '' : process.env.DEPLOYFU_MYSQL_PASSWORD;
 mysql.connect(function(err) {
   if (err)
     console.log(err);
